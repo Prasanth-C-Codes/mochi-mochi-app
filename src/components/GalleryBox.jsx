83947e5e82360221dc9ec2511480022cadb41965
Prasanth-C-Codes <jgs.prasanthc@gmail.com>
2023-08-30 12:03:46 +0530

    +Babel Devdependency & updated 'alt' in gallerybox

--- src/components/GalleryBox.jsx
+++ src/components/GalleryBox.jsx
@@ -4,10 +4,10 @@ export default function GalleryBox({ images }) {
   return (
     <div className="container py-3">
       <div className="row">
-        {images.map((image) => (
+        {images.map((image, index) => (
           <div className="col-lg-3 mb-3">
             <a href={image} data-fancybox="gallery">
-              <img src={image} alt="image" className="gallery-image" />
+              <img src={image} alt={index} className="gallery-image" />
             </a>
           </div>
         ))}
