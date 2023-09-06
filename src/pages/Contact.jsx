fbed3aaa6cd70fa94983a2813448c4fe0ef14e64
Selva <selvaarun555@gmail.com>
2023-09-06 05:43:20 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
                 {() => (
                   <Form>
                     <div className="form-group">
-                      <label htmlFor="name">Name:</label>
+                      <label htmlFor="name">Names:</label>
                       <Field
                         type="text"
                         className="form-control"
